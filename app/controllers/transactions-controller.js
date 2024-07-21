fb1c3c1f970450f4053cf0d3bb7f37326a0ab01c
Yaroslav Rashchupkin <yaroslav.rashchupkin.dev@gmail.com>
2024-07-22 01:44:28 +0300

    feat: juridical cash-out transaction

--- app/controllers/transactions-controller.js
+++ app/controllers/transactions-controller.js
@@ -1,5 +1,6 @@
 import fs from "fs";
 import { TransactionCashIn } from "../models/transaction-cash-in.js";
+import { TransactionCashOutJuridical } from "../models/transaction-cash-out-juridical.js";
 
 /**
  * Class representing a controller for managing transactions.
@@ -7,24 +8,23 @@ import { TransactionCashIn } from "../models/transaction-cash-in.js";
 export class TransactionsController {
   transactions = [];
 
-  /**
-   * Loads transactions from file and creates transactions instances.
-   */
   async loadTransactions(filePath) {
     const data = fs.readFileSync(filePath, "utf8");
     const transactions = JSON.parse(data);
 
     for (const transaction of transactions) {
-      if (transaction.type === "cash_in") {
+      const { type, user_type } = transaction;
+
+      if (type === "cash_in") {
         this.transactions.push(await TransactionCashIn.create(transaction));
+      } else if (type === "cash_out" && user_type === "juridical") {
+        this.transactions.push(
+          await TransactionCashOutJuridical.create(transaction),
+        );
       }
     }
   }
 
-  /**
-   * Maps the transactions to calculate each fee.
-   * @returns {[number]} - returns array of transactions fees
-   */
   processTransactions() {
     return this.transactions.map((transaction) => transaction.calculateFee());
   }
--- app/models/transaction-cash-out-juridical.js
+++ app/models/transaction-cash-out-juridical.js
@@ -1,4 +1,5 @@
 import { Transaction } from "./transaction.js";
+import { calculatePercentage, fetchRequest, roundDecimal } from "../utils.js";
 
 /**
  * Class representing a juridical cash-out transaction.
@@ -6,6 +7,48 @@ import { Transaction } from "./transaction.js";
  */
 export class TransactionCashOutJuridical extends Transaction {
   calculateFee() {
-    // Implementation of fee calculation goes here
+    const {
+      min: { amount: minFee },
+      percents,
+    } = this.config;
+
+    const {
+      operation: { amount },
+    } = this.transaction;
+
+    const fee = calculatePercentage(amount, percents);
+
+    return roundDecimal(Math.max(fee, minFee));
+  }
+
+  static async getConfig() {
+    if (!this.config) {
+      this.config = await fetchRequest(
+        "https://developers.paysera.com/tasks/api/cash-out-juridical",
+      );
+    }
+
+    return this.config;
+  }
+
+  static async create(transaction) {
+    this.validate(transaction);
+    const config = await this.getConfig();
+
+    return new TransactionCashOutJuridical(transaction, config);
+  }
+
+  static validate(transaction) {
+    const { type, user_type } = transaction;
+
+    if (type !== "cash_out") {
+      throw new Error(`${this.name}: provided type '${type}' is not valid.`);
+    }
+
+    if (user_type !== "juridical") {
+      throw new Error(
+        `${this.name}: provided user type '${user_type}' is not valid.`,
+      );
+    }
   }
 }
